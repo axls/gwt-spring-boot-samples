80b4a134e494bf44c120b8ce26ee48394e8a58b7
Alexander Leshkin <alexander.leshkin@gmail.com>
2018-08-19 13:07:32 +0300

    Added Polymer sample integration test.

--- gwt-spring-boot-sample-polymer/src/integTest/java/gwt/springboot/sample/polymer/PolymerIntegrationTest.java
+++ gwt-spring-boot-sample-polymer/src/integTest/java/gwt/springboot/sample/polymer/PolymerIntegrationTest.java
@@ -1,13 +1,30 @@
 package gwt.springboot.sample.polymer;
 
-import org.junit.Ignore;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.openqa.selenium.By.*;
+
 import org.junit.Test;
 
 import gwt.springboot.sample.test.BaseIntegrationTest;
 
 public class PolymerIntegrationTest extends BaseIntegrationTest {
 
-    @Test @Ignore
+    @Test
     public void test() throws InterruptedException {
+        var paperCheckbox = driver.findElement(xpath("//paper-checkbox"));
+        assertThat(paperCheckbox, notNullValue());
+        assertThat(paperCheckbox.getAttribute("checked"), is("true"));
+        
+        paperCheckbox.click();
+        assertThat(paperCheckbox.getAttribute("checked"), nullValue());
+        
+        var toggleCheckBoxButton = driver.findElement(xpath("//button[text()='Toggle']"));
+        toggleCheckBoxButton.click();
+        assertThat(paperCheckbox.getAttribute("checked"), is("true"));
+        
+        var customElement = driver.findElement(xpath("//custom-element"));
+        assertThat(customElement, notNullValue());
+        assertThat(customElement.getText(), is("Hello from GWT!"));
     }
 }
